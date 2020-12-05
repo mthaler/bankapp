aa0a1da3ba28394ce60cc71c7ffed6b1a17fcc83
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-12-05 14:52:36 +0100

    Updated BankApp.

--- autowiring/src/main/kotlin/com/mthaler/bankapp/BankApp.kt
+++ autowiring/src/main/kotlin/com/mthaler/bankapp/BankApp.kt
@@ -1,16 +1,15 @@
 package com.mthaler.bankapp
 
+import com.mthaler.bankapp.domain.FixedDepositDetails
+import com.mthaler.bankapp.service.AccountStatementService
+import com.mthaler.bankapp.service.CustomerRegistrationService
+import com.mthaler.bankapp.service.CustomerRequestService
+import com.mthaler.bankapp.service.FixedDepositService
 import kotlin.jvm.JvmStatic
 import org.springframework.context.ApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext
 import org.apache.logging.log4j.LogManager
-import com.mthaler.bankapp.service.CustomerRegistrationService
-import com.mthaler.bankapp.service.CustomerRequestService
-import com.mthaler.bankapp.domain.FixedDepositDetails
-import com.mthaler.bankapp.service.FixedDepositService
-import com.mthaler.bankapp.service.AccountStatementService
 import java.util.*
-
 
 object BankApp {
     private val logger = LogManager.getLogger(BankApp::class.java)
@@ -21,51 +20,48 @@ object BankApp {
             "classpath:META-INF/spring/applicationContext.xml"
         )
 
-        val fixedDepositService = context.getBean("service") as FixedDepositService
-
-        fixedDepositService.createFixedDeposit(
-            FixedDepositDetails(
-                101,
-                10000.0f, 60, "someemail@somedomain.com"
-            )
-        )
-
-        logger.info("Beginning with accessing CustomerRequestService")
+        logger.info("Beginning with accessing CustomerRequestService (autowire='constructor')")
         val customerRequestService_1 = context
-            .getBean(CustomerRequestService::class.java)
+            .getBean("customerRequestService") as CustomerRequestService
         customerRequestService_1.submitRequest(
             "checkBookRequest",
             "Request to send a 50-leaf check book"
         )
+        logger.info("Done with accessing CustomerRequestService (autowire='constructor')")
 
-        val customerRequestService_2 = context
-            .getBean(CustomerRequestService::class.java)
-        customerRequestService_2.submitRequest(
-            "checkBookRequest",
-            "Request to send a 100-leaf check book"
-        )
-        logger.info("Done with accessing CustomerRequestService")
-
-        logger.info("Beginning with accessing CustomerRegistrationService")
-
+        logger.info("Beginning with accessing CustomerRegistrationService (autowire='byType')")
         val customerRegistrationService_1 = context
-            .getBean(CustomerRegistrationService::class.java)
+            .getBean("customerRegistrationService") as CustomerRegistrationService
         customerRegistrationService_1.setAccountNumber("account_1")
         customerRegistrationService_1.setAddress("address_1")
         customerRegistrationService_1.setDebitCardNumber("debitCardNumber_1")
         customerRegistrationService_1.register()
+        logger.info("Done with accessing CustomerRegistrationService (autowire='byType')")
 
-        logger.info("registered customer with id account_1")
-
+        logger.info("Beginning with accessing CustomerRegistrationService (autowire='no')")
         val customerRegistrationService_2 = context
-            .getBean(CustomerRegistrationService::class.java)
-        customerRegistrationService_2.setAccountNumber("account_2")
-        customerRegistrationService_2.setAddress("address_2")
-        customerRegistrationService_2.setDebitCardNumber("debitCardNumber_2")
-        customerRegistrationService_2.register()
-        logger.info("registered customer with id account_2")
+            .getBean("customerRegistrationService_") as CustomerRegistrationService
+        try {
+            customerRegistrationService_2.setAccountNumber("account_2")
+            customerRegistrationService_2.setAddress("address_2")
+            customerRegistrationService_2
+                .setDebitCardNumber("debitCardNumber_2")
+            customerRegistrationService_2.register()
+        } catch (e: Exception) {
+            logger.error("Exception : $e")
+        }
+        logger.info("Done with accessing CustomerRegistrationService (autowire='no')")
 
-        logger.info("Done with accessing CustomerRegistrationService")
+        logger.info("Beginning with accessing FixedDepositService (autowire='byName')")
+        val fixedDepositService = context
+            .getBean(FixedDepositService::class.java)
+        fixedDepositService.createFixedDeposit(
+            FixedDepositDetails(
+                1, 1000.0f,
+                12, "someemail@somedomain.com"
+            )
+        )
+        logger.info("Done with accessing FixedDepositService (autowire='byName')")
 
         logger.info("Beginning with accessing AccountStatementService (autowire-candidate='false')")
         try {
