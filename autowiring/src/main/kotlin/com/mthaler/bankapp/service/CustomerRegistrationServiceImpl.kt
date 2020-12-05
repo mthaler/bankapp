bc2fde0aebaa5766939c9177211ac88e3c07a32d
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-12-05 14:55:23 +0100

    Updated CustomerRegistrationServiceImpl.

--- autowiring/src/main/kotlin/com/mthaler/bankapp/service/CustomerRegistrationServiceImpl.kt
+++ autowiring/src/main/kotlin/com/mthaler/bankapp/service/CustomerRegistrationServiceImpl.kt
@@ -1,39 +1,26 @@
 package com.mthaler.bankapp.service
-import java.beans.ConstructorProperties
+
 import com.mthaler.bankapp.domain.CustomerRegistrationDetails
 import com.mthaler.bankapp.dao.CustomerRegistrationDao
-import org.apache.logging.log4j.LogManager
 
-class CustomerRegistrationServiceImpl @ConstructorProperties(
-    "customerRegistrationDetails",
-    "customerRegistrationDao"
-) constructor(
-    private val customerRegistrationDetails: CustomerRegistrationDetails,
-    private val customerRegistrationDao: CustomerRegistrationDao
-) : CustomerRegistrationService {
+class CustomerRegistrationServiceImpl : CustomerRegistrationService {
 
-    init {
-        logger.info("Created CustomerRegistrationServiceImpl instance")
-    }
+    var customerRegistrationDetails: CustomerRegistrationDetails? = null
+    var customerRegistrationDao: CustomerRegistrationDao? = null
 
     override fun setAccountNumber(accountNumber: String) {
-        customerRegistrationDetails.accountNumber = accountNumber
+        customerRegistrationDetails!!.accountNumber = accountNumber
     }
 
     override fun setAddress(address: String) {
-        customerRegistrationDetails.setAddress(address)
+        customerRegistrationDetails!!.setAddress(address)
     }
 
     override fun setDebitCardNumber(cardNumber: String) {
-        customerRegistrationDetails.setCardNumber(cardNumber)
+        customerRegistrationDetails!!.setCardNumber(cardNumber)
     }
 
     override fun register() {
-        customerRegistrationDao.registerCustomer(customerRegistrationDetails)
-    }
-
-    companion object {
-
-        private val logger = LogManager.getLogger(CustomerRegistrationServiceImpl::class.java)
+        customerRegistrationDao!!.registerCustomer(customerRegistrationDetails!!)
     }
 }
