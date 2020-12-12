6c352c8b392c94713884d182a92ae7d5b2ca4c2e
Michael Thaler <michael.thaler@physik.tu-muenchen.de>
2020-12-12 16:53:48 +0100

    Added CustomerRequestDao and CustomerRequestDaoImpl.

---
+++ annotations/src/main/kotlin/com/mthaler/bankapp/dao/CustomerRequestDaoImpl.kt
@@ -0,0 +1,24 @@
+package com.mthaler.bankapp.dao
+
+import com.mthaler.bankapp.dao.CustomerRequestDao
+import com.mthaler.bankapp.domain.CustomerRequestDetails
+import com.mthaler.bankapp.dao.CustomerRequestDaoImpl
+import org.apache.logging.log4j.LogManager
+import org.springframework.stereotype.Repository
+
+@Repository(value = "customerRequestDao")
+class CustomerRequestDaoImpl : CustomerRequestDao {
+    
+    init {
+        logger.info("Created CustomerRequestDaoImpl instance")
+    }
+
+    override fun submitRequest(userRequestDetails: CustomerRequestDetails) {
+        // --save UserRequestDetails
+    }
+
+    companion object {
+        private val logger = LogManager
+            .getLogger(CustomerRequestDaoImpl::class.java)
+    }
+}
